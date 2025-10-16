cb35b24d43bf3e2e9b16e24bd8773569625a95cc
Matthew Watkins <mwatkins@linuxfoundation.org>
2025-10-16 15:53:50 +0100

    Fix: Builds need pin Python <3.14
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- src/gha_workflow_linter/_version.py
+++ src/gha_workflow_linter/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.4.dev0+gf0c6e2444.d20251004'
-__version_tuple__ = version_tuple = (0, 1, 4, 'dev0', 'gf0c6e2444.d20251004')
+__version__ = version = '0.1.6.dev0+g6c79417fe.d20251016'
+__version_tuple__ = version_tuple = (0, 1, 6, 'dev0', 'g6c79417fe.d20251016')
 
 __commit_id__ = commit_id = None
